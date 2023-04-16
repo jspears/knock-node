a85739f4bd45596f04e13fd3831aeda04d3cf040
Justin Spears <speajus@gmail.com>
2023-04-15 21:52:15 -0700

    make version a file

--- denoify.config.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-
-/** @type { import('denoify/lib/config/parseParams').DenoifyParams } */
-const config = {
-    "includes":["README","LICENSE", "./package.json"]
-    //...your options
-}
-module.exports = config;
