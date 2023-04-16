387ada290f350907748e5edc333081907b371969
Justin Spears <speajus@gmail.com>
2023-04-15 21:27:54 -0700

    fix package.json

---
+++ denoify.config.js
@@ -0,0 +1,8 @@
+// @ts-check
+
+/** @type { import('denoify/lib/config/parseParams').DenoifyParams } */
+const config = {
+    "includes":["README","LICENSE", "./package.json"]
+    //...your options
+}
+module.exports = config;
--- src/package.ts
+++ src/package.ts
@@ -1,15 +1,18 @@
 import * as fs from "fs";
 import * as path from "path";
 import { TextDecoder } from "util";
-export function getPackage(): Record<string,any> {
-    return JSON.parse(
-        new TextDecoder("utf-8").decode(
-            fs.readFileSync(
-                path.join(
-                    __dirname,
-                    "..", "package.json"
+export function getPackage(): Record<string,unknown> {
+    for(const dir of [".", ".."]) {
+        const file = path.join(__dirname, dir, "package.json");
+        if (fs.existsSync(file)) {
+            return JSON.parse(
+                new TextDecoder("utf-8").decode(
+                    fs.readFileSync(
+                       file
+                    ) as Uint8Array
                 )
-            ) as Uint8Array
-        )
-    );
+            );
+        }
+    }
+    throw new Error(`Could not find package.json in ${__dirname} or ${path.join(__dirname, "..")}`);
 }
