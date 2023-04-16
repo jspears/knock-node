8f963776d41ffc41dce8d8a205a32bf14c9e63d6
Justin Spears <speajus@gmail.com>
2023-04-15 22:41:40 -0700

    make work for supabase

--- src/port.deno.ts
+++ src/port.deno.ts
@@ -1,4 +1,4 @@
-export { emitWarning } from "https://deno.land/std@0.172.0/node/process.ts";
 export function getEnv(key:string):string | undefined{
     return Deno.env.get(key);
 }
+export const emitWarning = console.warn.bind(console);
